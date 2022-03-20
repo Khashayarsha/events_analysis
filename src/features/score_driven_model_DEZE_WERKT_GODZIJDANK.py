0e9ad9bf7777e48e8f65c7dbe085baf5cc9cca05
XHK <Khashayarsha@users.noreply.github.com>
2022-03-20 02:40:20 +0100

    Update score_driven_model_DEZE_WERKT_GODZIJDANK.py
    zz

--- src/features/score_driven_model_DEZE_WERKT_GODZIJDANK.py
+++ src/features/score_driven_model_DEZE_WERKT_GODZIJDANK.py
@@ -167,7 +167,7 @@ def get_strengths_dictionary(f1, variable_names = 'goals'):
         ft = {team: [f1_goals.get(team)] for team in f1_goals.keys()}
         for team in non_first_partic:
             ft.update({team: [(0, 0)]})
-    if variable_names not in ['goals', 'attempts', 'weighted_attempts']:
+    if variable_names not in ['goals', 'attempts', 'weighted_attempts_discrete']:
         print(f"SDM get_strengths_dict says: Variable name {variable_names} not recognized.")
 
     return deepcopy(ft)
@@ -191,19 +191,144 @@ def get_w (f1):
 #@CallCountDecorator
 
 
+def run_model_extended3(params, *args):
+
+    # ('goals weighted_attempts', False, ft_goals, ft_weighted_attempts, 1) = args
+    variable_names, return_strengths, ft_local_goals, ft_local_other, extension_type = args
+    var_name1, var_name2 = variable_names.split()    #'goals', 'weighted_attempts'
+    if extension_type == 1:
+        # a1, a2, b1, b2, delta, l3, eta = params
+        a1,  a2, a3, a4,  delta1, delta2, l3, theta3, eta1 = params
+    if extension_type == 2:
+        #a1, a2, b1, b2, delta,l3, eta1, eta2  = params
+        a1,  a2, a3, a4,  delta1, delta2, l3, theta3, eta1, eta2 = params
+    # if return_strengths:
+    #     match_id_dict = {match_id: {'home': [], 'away': []}
+    #                      for match_id in df.id_odsp}
+    b1 = 1
+    b2 = 1  # set to 1
+    b3 = 1
+    b4 = 1
+
+    ft_local_goals = deepcopy(ft_local_goals)
+    ft_local_other = deepcopy(ft_local_other)
 
 
+    total_likelihood = 0
+
+    for round in set(latter_seasons.round_labels):
+        round_matches = latter_seasons[latter_seasons.round_labels == round]
+
+        round_participants = set(round_matches.iloc[0].participants)
+
+        for index, match in round_matches.iterrows():
+            home_team = match['ht']
+            away_team = match['at']
+
+            h_col_name, a_col_name = 'home_'+var_name1, 'away_'+var_name1            
+            home_result = int(match[h_col_name])
+            away_result = int(match[a_col_name])
+            #home_weighted_attempts = match_id_dict[match.id_odsp]['home']
+            #away_weighted_attempts = match_id_dict[match.id_odsp]['away']
+            home_strengths = alpha_i, beta_i = ft_local_goals[home_team][-1][0:2]
+            away_strengths = alpha_j, beta_j = ft_local_goals[away_team][-1][0:2]
+
+
+            h_col_name_other, a_col_name_other = 'home_'+var_name2, 'away_'+var_name2            
+            home_result_other = int(match[h_col_name_other])
+            away_result_other = int(match[a_col_name_other])
+            home_strengths_other = gamma_i, nu_i = ft_local_other[home_team][-1][0:2]
+            away_strengths_other = gamma_j, nu_j = ft_local_other[away_team][-1][0:2]
+
+            # if return_strengths:           This is used in the other run_model to construct the id-dict
+            #     match_id_dict[match.id_odsp]['home'].append(
+            #         ft_local[home_team][-1][0:2])
+            #     match_id_dict[match.id_odsp]['away'].append(
+            #         ft_local[away_team][-1][0:2])
+
+            #delta = delta #+ get_exogenous(match)
+            if extension_type == 1:
+                l1, l2 = biv_poiss.link_function_ext1(
+                    alpha_i, alpha_j, beta_i, beta_j, delta1, eta1, gamma_i, nu_j)
+            if extension_type == 2:
+                l1, l2 = biv_poiss.link_function_ext2(
+                    alpha_i, alpha_j, beta_i, beta_j, delta1, eta1, eta2, gamma_i, gamma_j, nu_i, nu_j)
+
+            if extension_type == 1 or extension_type == 2:
+                theta1, theta2 = biv_poiss.link_function(gamma_i, gamma_j, nu_i, nu_j, delta2)
+            else:
+                print(f"extension type {extension_type} not recognized")
+
+
+            # l1 = math.exp(home_adv + home_strengths[0] - away_strengths[1])
+            # l2 = math.exp(away_strengths[0] - home_strengths[1])
+
+            goals_score = biv_poiss.score(home_result, away_result, l1, l2, l3)
+
+            alpha_i_next = 0 + b1*home_strengths[0] + a1*goals_score[0]
+            alpha_j_next = 0 + b1*away_strengths[0] + a1*goals_score[1]
+            beta_i_next = 0 + b2 * home_strengths[1] + a2*goals_score[2]
+            beta_j_next = 0 + b2*away_strengths[1] + a2*goals_score[3]
+
+            other_score = biv_poiss.score(home_result_other, away_result_other, theta1, theta2, theta3)
+
+            gamma_i_next = 0+ b3*home_strengths_other[0] + a3* other_score[0]
+            gamma_j_next = 0+ b3*home_strengths_other[0] + a3* other_score[1]
+            nu_i_next =  0+ b4* home_strengths_other[1] + a4 *other_score[2]
+            nu_j_next = 0 + b4*home_strengths_other[1] + a4 * other_score[3]
+
+
+            ft_local_goals[home_team].append(
+                (alpha_i_next, beta_i_next, round, match.id_odsp, 'home'))
+            ft_local_goals[away_team].append(
+                (alpha_j_next, beta_j_next, round, match.id_odsp, 'away'))
+
+            ft_local_other[home_team].append(
+                (gamma_i_next, nu_i_next, round, match.id_odsp, 'home'))
+            ft_local_other[away_team].append(
+                (gamma_j_next, nu_j_next, round, match.id_odsp, 'away'))
+
+
+            #results_dict[match.id_odsp]['h_est_'+variable_names].append()
+
+            p = biv_poiss.prob_mass_func(home_result, away_result, l1, l2, l3)
+
+            if p > 0:
+                total_likelihood = total_likelihood + math.log(p)
+
+        for team in all_teams.difference(round_participants):
+            alpha_i_next = b1*ft_local_goals[team][-1][0]
+            beta_i_next = b2*ft_local_goals[team][-1][1]
+
+            gamma_i_next = b3* ft_local_other[team][-1][0]
+            nu_i_next = b4 * ft_local_other[team][-1][1]
+
+
+            ft_local_goals[team].append((alpha_i_next, beta_i_next, round, 'absent'))
+            ft_local_other[team].append( (gamma_i_next, nu_i_next, round, 'absent'))
+            #ft_local[team].append(ft_local[team][-1])
+
+    if return_strengths:
+        return ft_local_goals, ft_local_other,  -total_likelihood
+    return -total_likelihood
 
 
 
 def run_model_extended(params, *args):
 
-    variable_names, return_strengths, ft_local,  match_id_dict = args # df, latter_seasons, all_teams, match_id_dict = args
-    a1, a2, b1, b2, delta, l3, eta = params
+    variable_names, return_strengths, ft_local,  match_id_dict, extension_type = args # df, latter_seasons, all_teams, match_id_dict = args
+    if extension_type == 1:
+        # a1, a2, b1, b2, delta, l3, eta = params
+         a1, a2, delta, l3, eta = params
+    if extension_type == 2:
+        #a1, a2, b1, b2, delta,l3, eta1, eta2  = params
+        a1, a2, delta, l3, eta1, eta2 = params
     # if return_strengths:
     #     match_id_dict = {match_id: {'home': [], 'away': []}
     #                      for match_id in df.id_odsp}
-
+    b1 = 1 
+    b2 = 1  #set to 1 
+    ft_local = deepcopy(ft_local)
     total_likelihood = 0
 
     for round in set(latter_seasons.round_labels):
@@ -223,19 +348,22 @@ def run_model_extended(params, *args):
 
             home_strengths = alpha_i, beta_i = ft_local[home_team][-1][0:2]
             away_strengths = alpha_j, beta_j = ft_local[away_team][-1][0:2]
-            home_wa_strengths = gamma_i, nu_i = match_id_dict[match.id_odsp]['home']
-            away_wa_strengths = gamma_j, nu_j = match_id_dict[match.id_odsp]['away']
+            home_wa_strengths = gamma_i, nu_i = match_id_dict[match.id_odsp]['home'][0]
+            away_wa_strengths = gamma_j, nu_j = match_id_dict[match.id_odsp]['away'][0]
 
-            if return_strengths:
-                match_id_dict[match.id_odsp]['home'].append(
-                    ft_local[home_team][-1][0:2])
-                match_id_dict[match.id_odsp]['away'].append(
-                    ft_local[away_team][-1][0:2])
+            # if return_strengths:           This is used in the other run_model to construct the id-dict
+            #     match_id_dict[match.id_odsp]['home'].append(
+            #         ft_local[home_team][-1][0:2])
+            #     match_id_dict[match.id_odsp]['away'].append(
+            #         ft_local[away_team][-1][0:2])
 
             #delta = delta #+ get_exogenous(match)
+            if extension_type == 1:
+                l1, l2 = biv_poiss.link_function_ext1(
+                    alpha_i, alpha_j, beta_i, beta_j, delta, eta, gamma_i, nu_j)
+            if extension_type == 2:
+                l1, l2 = biv_poiss.link_function_ext2(alpha_i, alpha_j, beta_i, beta_j, delta, eta1, eta2, gamma_i, gamma_j, nu_i, nu_j)
 
-            l1, l2 = biv_poiss.link_function_ext1(
-                alpha_i, alpha_j, beta_i, beta_j, delta, eta, gamma_i, nu_j)
 
             # l1 = math.exp(home_adv + home_strengths[0] - away_strengths[1])
             # l2 = math.exp(away_strengths[0] - home_strengths[1])
@@ -274,7 +402,7 @@ def run_model(params, *args):
     #init teams that aren't playing in first year to be 0 
     variable_names, return_strengths = args
     if variable_names == 'goals':
-        a1, a2, b1, b2, delta, l3 = params
+        a1, a2, delta, l3 = params #a1, a2, b1, b2, delta, l3 = params
         ft_local = deepcopy(ft_goals)
     if variable_names == 'attempts':
         a1, a2, b1, b2, delta, l3 = params
@@ -290,6 +418,9 @@ def run_model(params, *args):
     if return_strengths: 
         match_id_dict = {match_id:{'home':[] , 'away': [] } for match_id in df.id_odsp}
 
+    if variable_names == 'goals':        
+        b1 = 1
+        b2 = 1
 
     total_likelihood = 0 
 
@@ -367,37 +498,91 @@ ft_goals = get_strengths_dictionary(f1_goals, variable_names='goals')
 # opt = optimize.minimize(run_model, x0_b,args = args, method='BFGS')
 # strengths_opt, match_dict_goals, LL = run_model(opt.x, 'goals', True)
 #g_optimum = [0.02056461, 0.01779263, 0.99449382, 0.99792438, 0.34076973, 0.02790289]
-g_optimum = [0.02056461, 0.01779263, 0.99449382,
-             0.99792438, 0.34076973, 0.02790289]
+g_optimum = [0.02056461, 0.01779263, 0.34076973, 0.02790289]  # [0.02056461, 0.01779263, 0.99449382,             0.99792438, 0.34076973, 0.02790289]
 strengths_opt, match_dict_goals, LL = run_model(g_optimum, 'goals', True)
-# print(f"Estimating model for ATTEMPTS")
-#ft_attempts = get_strengths_dictionary(f1_attempts,variable_names = 'attempts')
+
+
+
+# print(f"Estimating model for ATTEMPTS-----------------------------------------------------------")
+# ft_attempts = get_strengths_dictionary(f1_attempts,variable_names = 'attempts')
 # args = ('attempts', False)
 # opt_attempts = optimize.minimize(run_model, x0_b, args=args)
-# attempt_strengths_opt, LL2 = run_model(opt_attempts.x, 'attempts', True)
+# attempt_strengths_opt, match_dict_attempts, LL2 = run_model(opt_attempts.x, 'attempts', True)
 
-
-# print(f"Estimating constrained model for ATTEMPTS")
+ 
+# print(f"Estimating constrained model for ATTEMPTS-----------------------------------------------")
 # ft_attempts = get_strengths_dictionary(f1_attempts, variable_names='attempts')
 # args = ('attempts', False)
 
 
 # bnds = [(-2, 2), (-2, 2), (-0.99, 0.99),
-#         (-0.99, 0.99), (15, 15), (-0.99, 0.99) ]
-# opt_attempts = optimize.minimize(run_model, x0_b, args=args, method='L-BFGS-B', bounds=bnds, tol=None, callback=None, options={
+#         (-0.99, 0.99), (-30, 100), (-0.99, 100) ]
+# opt_attempts = optimize.minimize(run_model, x0_b, args=args, method='SLSQP', bounds=bnds, tol=None, callback=None, options={
 #                                        'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
-# attempt_strengths_opt, LL2 = run_model(opt_attempts.x, 'attempts', True)
+# attempt_strengths_opt, match_dict_attempts, LL2 = run_model(opt_attempts.x, 'attempts', True)
+
+# print(f"Estimating model for WEIGHTED ATTEMPTS-----------------------------------------------------------")
 ft_weighted_attempts = get_strengths_dictionary(f1_weighted_attempts, variable_names='weighted_attempts_discrete')
-wa_optimum = [0.05960302, 0.09505286, 0.98744643,
-              0.99708252, 0.66970482,  0.63086085]
-#print(f"Estimating model for WEIGHTED ATTEMPTS")
-weighted_attempt_strengths_opt, match_dict_weighted_attempts,  LL3 = run_model(
-    wa_optimum, 'weighted_attempts_discrete', True)
+# wa_optimum = [0.05960302, 0.09505286, 0.98744643,
+#               0.99708252, 0.66970482,  0.63086085]
+# #print(f"Estimating model for WEIGHTED ATTEMPTS")
+# weighted_attempt_strengths_opt, match_dict_weighted_attempts,  LL3 = run_model(
+#     wa_optimum, 'weighted_attempts_discrete', True)
+
+
+
 #args = ('weighted_attempts_discrete', False)
 #opt_weighted_attempts = optimize.minimize(run_model, x0_b, args = args)
 #weighted_attempt_strengths_opt, match_dict_weighted_attempts,  LL3 = run_model(opt_weighted_attempts.x, 'weighted_attempts_discrete', True)
 #args_opt = (variable_names, True)
 
+# variable_names, return_strengths, ft_local,  match_id_dict, extension_type
+
+# print('estimating model extension 1 (goals, weighted_attempts eta1) -----------------------------------------------------------------------')
+# args = ('goals', False, ft_goals,  match_dict_weighted_attempts, 1 ) #(var_names, return_strengths, ft_local,  match_id_dict )
+# x0_extended = [0.0205, 0.0178, 0.3407, 0.02790,0.4]  # a1, a2, b1, b2, delta, l3, eta = params
+
+# bnds = [(-1, 1), (-1, 1), (0, 5),    #a1 a2 delta 
+#         (0, 5), (0, 5)]      # l3 eta
+# extended_strengths_opt = optimize.minimize(run_model_extended, x0_extended, args=args, method='L-BFGS-B', bounds=bnds, tol=None, callback=None, options={
+#     'disp': None, 'maxcor': 10, 'ftol': 2.220446049250313e-09, 'gtol': 1e-05, 'eps': 1e-08, 'maxfun': 15000, 'maxiter': 15000, 'iprint': - 1, 'maxls': 20, 'finite_diff_rel_step': None})
+# #extended_strengths_opt = optimize.minimize(run_model_extended, x0_extended, args=args, method='BFGS')
+# extended_strengths_opt_dict, match_dict_extended, LL4 = run_model_extended(
+#     extended_strengths_opt.x, 'goals', True, ft_goals,  match_dict_weighted_attempts, 1)
+# # extended_strengths_opt.x = [ 0.020,  0.0177, 0.9946, 0.9979,  0.3257, 0.01853, -0.01364  ]
+#                             #   a1       a2      b1       b2     delta     l3       eta
+
+
+# print('estimating model extension 2 (goals, weighted_attemts, eta1, eta2------------------------')
+# args2 = ('goals', False, ft_goals,  match_dict_weighted_attempts, 2)  # (var_names, return_strengths, ft_local,  match_id_dict )
+# x0_extended2 = [0.0205, 0.0178,  0.3407, 0.02790, 0.4,0.4]  # a1, a2, b1, b2, delta, l3, eta1,eta2 = params
+
+# extended_strengths_opt2 = optimize.minimize(
+#     run_model_extended, x0_extended2, args=args2, method='BFGS')
+# extended_strengths_opt2_dict, match_dict_extended, LL5 = run_model_extended(
+#     extended_strengths_opt2.x, 'goals', True, ft_goals,  match_dict_weighted_attempts ,2)
+
+
+print('estimating model extension 3  (simulataneous estimation) ------------------------')
+# (var_names, return_strengths, ft_local,  extension_type )
+args3 = ('goals weighted_attempts', False, ft_goals, ft_weighted_attempts, 1)
+# a1, a2, b1, b2, delta, l3, eta1,eta2 = params
+
+x0_extended3 = [0.02, 0.02, 0.02, 0.02,  0.3, 0.3,  0.05, 0.05, 0.05] #, 0.5]  # 9 params. 10 with eta2
+                #a1,  a2 , a3, a4 ,delta1,delta2,l3,     theta3,eta1 (eta2)
+bnds = [(-1, 1), (-1, 1), (-1, 1), (-1, 1), (-10, 10), (-10, 10), (0, 1), (0,1), (-10,10)]
+import scipy
+#extended_strengths_opt3 =scipy.optimize.differential_evolution(run_model_extended3, bounds =bnds, args=args3, strategy='best1bin', maxiter=50, popsize=15, tol=0.01, mutation=(0.5, 1), recombination=0.7, seed=None, callback=None, disp=False, polish=True, init='latinhypercube', atol=0, updating='immediate', workers=1, constraints=())
+
+extended_strengths_opt3 = optimize.minimize(run_model_extended3, x0_extended3, args=args3, method='BFGS')
+
+
+goal_strengths_simultaan_dict, weighted_strengths_simultaan_dict, LL6 = run_model_extended3(
+    extended_strengths_opt3.x, 'goals weighted_attempts', True, ft_goals,  ft_weighted_attempts, args3[-1])
+
+
+# optimum (quasi) found for extended model 3 (simultaneous) with extension_nr = 2.
+#[ 0.02489183,  0.02296556, -0.01361026,  0.27460132,  0.34403237, 0.23702291,  0.05186831, -0.02307723,  0.02760914,  0.02124028
 
 names = 'goals weighted_attempts_discrete'.split()
 exo_dict = {name: {team_name:{} for team_name in all_teams} for name in names }
@@ -406,12 +591,12 @@ exo_dict = {name: {team_name:{} for team_name in all_teams} for name in names }
 
 #attempt_strengths_opt, LL2 = run_model(x_opt_attempts, 'weighted_attempts_discrete', True)
 
-def plot_dict_entry(ft,team, variable_names = 'goals'):
+def plot_dict_entry(ft, team, variable_names='goals', save = False):
     # if variable_names == 'goals':
     #     attack = [i[0] for i in ft[team]]
     #     defense = [i[1] for i in ft[team]]
     #     x = pd.DataFrame([attack, defense]).T
-    #     x.columns = ['attack_goal','defense_goal']        
+    #     x.columns = ['attack_goal','defense_goal']
     #     x.plot(title = team+'__'+variable_names)
 
     # if variable_names == 'weighted_attempts_discrete':
@@ -420,18 +605,27 @@ def plot_dict_entry(ft,team, variable_names = 'goals'):
     #     x = pd.DataFrame([attack, defense]).T
     #     x.columns = ['attack_weighted_attempt', 'defense_weighted_attempt']
     #     x.plot(title=team+'__'+variable_names)
-    # else: 
+    # else:
     attack = [i[0] for i in ft[team]]
     defense = [i[1] for i in ft[team]]
     x = pd.DataFrame([attack, defense]).T
     x.columns = ['attack_'+variable_names, 'defense_'+variable_names]
     x.plot(title=team+'__'+variable_names)
-
+    fig = x.plot(title=team+'__'+variable_names).get_figure()
+    if save != False:
+        fig.savefig(team+'__'+variable_names+'.png', dpi=400)
 
 
 plot_dict_entry(strengths_opt,'Lyon')
 #plot_dict_entry(attempt_strengths_opt, 'Lyon', 'attempts')
-plot_dict_entry(weighted_attempt_strengths_opt, 'Lyon', 'weighted_attempts_discrete')
+# plot_dict_entry(weighted_attempt_strengths_opt, 'Lyon', 'weighted_attempts_discrete')
+# plot_dict_entry(extended_strengths_opt_dict,
+#                 'Lyon', 'extended_model_goals')
+# plot_dict_entry(extended_strengths_opt2_dict, 'Lyon', 'extended_model_2_goals')
+
+plot_dict_entry(goal_strengths_simultaan_dict, 'Lyon', 'goals_simultaneous extended')
+plot_dict_entry(weighted_strengths_simultaan_dict, 'Lyon', 'weighted_attempts simultaneous extended')
+
 
 # def game_likelihood():
 #     return 0
@@ -452,6 +646,27 @@ plot_dict_entry(weighted_attempt_strengths_opt, 'Lyon', 'weighted_attempts_discr
 #run optimizer on above 
 
 #plot and compare to Koopman/Lit
+
+save_dict2 = {'x0': x0_extended3, 'optimizer_results': extended_strengths_opt3,
+              'goal_strengths': goal_strengths_simultaan_dict, 'wa_strengths': weighted_strengths_simultaan_dict, 'likelihood': LL6}
+
+from scipy import stats
+from scipy.stats.distributions import chi2
+LL1 = -LL
+LL2 = -LL6
+
+
+def likelihood_ratio(llmin, llmax):
+
+    return 2*(llmax-llmin)
+
+
+LR = likelihood_ratio(LL1, LL2)
+dof = 6  # Degrees of freedom
+p = chi2.sf(LR, dof)
+
+print('LR: ', LR, 'p: ', p)
+
 
 
 # %%
